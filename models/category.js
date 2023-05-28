0fd1ad257b0672fefa5bc987eea2b3c18a8066fc
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-05-27 22:25:27 -0700

    Define schema models for data

---
+++ models/category.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 280 },
+});
+
+CategorySchema.virtual('url').get(function () {
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 280 },
+  category: { type: Schema.Types.ObjectId, required: true },
+  price: { type: Number, required: true, min: 0 },
+  instock: { type: Number, required: true, min: 0 },
+});
+
+ItemSchema.virtual('url').get(function () {
+  return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
